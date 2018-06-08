da99d99b267b53fc4e0982facbdd7245091a55ab
calebherne <calebherne@gmail.com>
2018-06-07 22:08:03 -0400

    fixed gradebook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,17 +19,16 @@ namespace GradeBook.GradeBooks
             var threshold = (int)Math.Ceiling(Students.Count * .2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (grades[threshold - 1] <= averageGrade)
+            if (grades[threshold-1] <= averageGrade)
                 return 'A';
-            else if (grades[(threshold - 1) * 2] <= averageGrade)
+            else if (grades[threshold*2-1] <= averageGrade)
                 return 'B';
-            else if (grades[(threshold - 1) * 3] <= averageGrade)
+            else if (grades[threshold*3-1] <= averageGrade)
                 return 'C';
-            else if (grades[(threshold - 1) * 4] <= averageGrade)
+            else if (grades[threshold*4-1] <= averageGrade)
                 return 'D';
             else
                 return 'F';
-            return base.GetLetterGrade(averageGrade);
         }
     }
 }
